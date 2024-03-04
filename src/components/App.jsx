491092e834a4e8896cacda476af42e38a37e7731
MShmatok <91060161+MShmatok@users.noreply.github.com>
2024-03-04 20:43:32 +0200

    done fix button type

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
 import { lazy } from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './layout/Layout';
+import { useSelector } from 'react-redux';
+import { selectIsLoading } from '../redux/root/selectors';
+import Loader from './loader/Loader';
 
 const Shop = lazy(() => import('../pages/shop/Shop'));
 const Cart = lazy(() => import('../pages/cart/Cart'));
-const History = lazy(() => import('../pages/history/History'));
+// const History = lazy(() => import('../pages/history/History'));
 
 const NotFoundPage = lazy(() => import('../pages/notFoundPage/NotFoundPage'));
-
 const createRouter = () => {
   const router = createBrowserRouter(
     [
@@ -24,11 +26,11 @@ const createRouter = () => {
             element: <Cart />,
             errorElement: <NotFoundPage />,
           },
-          {
-            path: '/history',
-            element: <History />,
-            errorElement: <NotFoundPage />,
-          },
+          // {
+          //   path: '/history',
+          //   element: <History />,
+          //   errorElement: <NotFoundPage />,
+          // },
 
           {
             path: '*',
@@ -44,5 +46,11 @@ const createRouter = () => {
 };
 
 export const App = () => {
-  return <RouterProvider router={createRouter()} />;
+  const isLoading = useSelector(selectIsLoading);
+
+  return (
+    <>
+      <RouterProvider router={createRouter()} />;{isLoading && <Loader />}
+    </>
+  );
 };
